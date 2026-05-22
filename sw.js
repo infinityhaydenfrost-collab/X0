1fba8ca2ee3b66528f09def486deae66efc7dd80
infinityhaydenfrost-collab <infinityhaydenfrost@gmail.com>
2026-05-22 21:55:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "notes-app-v13";
+const CACHE_NAME = "notes-app-v15";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -32,6 +32,12 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
